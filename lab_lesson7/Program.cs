889f4def220e5608215a28855a50ea185e1d2e2f
Никита Макурин <makurin.nik@list.ru>
2021-10-26 17:17:31 +0300

    1

--- lab_lesson7/Program.cs
+++ lab_lesson7/Program.cs
@@ -23,15 +23,40 @@ namespace lab_lesson7
         {
             get { return author; }
         }
-        //public string Title()
-        //{
+        public string Title()
+        {
+            return name + " " + author;
+        }
 
-        //}
-        ////метод, который сравнивает между собой два объекта-песни:
-        //public bool  Equals(object d)
-        //{
-        //    return ;
-        //}
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            Song song = obj as Song;
+            return (this.name == song.name) && (this.author == song.author);
+        }
+        public static void SearchEqualsSongs(List<Song> songs)
+        {
+            bool isFonded = false;
+            for (int i = 0; i < songs.Count; i++)
+            {
+                for (int j = i + 1; j < songs.Count; j++)
+                {
+                    if (songs[i].Equals(songs[j]))
+                    {
+                        isFonded = true;
+                        Console.WriteLine($"Совпали песни под номерами {i + 1} и {j + 1}");
+                        Console.WriteLine($"Песня под названием {songs[i].name} , автор {songs[i].author}");
+                    }
+                }
+            }
+            if (!isFonded)
+            {
+                Console.WriteLine("Одинаковых песен не найдено");
+            }
+        }
     }
         internal class Program
     {
